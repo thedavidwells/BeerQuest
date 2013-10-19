f5a55bd045df016eb8ffdaf6977462c45b90ed89
David Wells <wellsland@gmail.com>
2013-10-19 14:41:15 -0700

    Added a player property so that we can add a player to the game.

--- BeerQuest/MyScene.m
+++ BeerQuest/MyScene.m
@@ -8,22 +8,24 @@
 
 #import "MyScene.h"
 
+@interface MyScene ()
+
+@property (nonatomic) SKSpriteNode * player;
+
+@end
+
+
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
+        NSLog(@"Size: %@", NSStringFromCGSize(size));
+        
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
     }
     return self;
 }
